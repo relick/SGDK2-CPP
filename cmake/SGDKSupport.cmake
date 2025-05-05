d490844bc5e995c7515c6bba0999dfb2786e07af
relick <relick@users.noreply.github.com>
2025-05-05 22:53:11 +0100

    CMake: Improve how z80 assembly and resource compilation is handled
    - dep file generated by rescomp now provided to CMake
    - target_sources with a headers FILE_SET generates include directories for BUILD_INTERFACE, so all the extra target_include_directories for the build folders have been removed
    - z80 includes now programmatically determined from the provided target allowing it to be fully portable with an install.
    - function md_target_z80_sources_with_inc added as a way to specify a second target just for z80 assembly includes, allowing these to be kept separate and not be inherited by the main target

--- cmake/SGDKSupport.cmake
+++ cmake/SGDKSupport.cmake
@@ -2,7 +2,11 @@ cmake_minimum_required(VERSION 4.0.1)
 
 # Rules to build Z80 source code
 function(md_target_z80_sources target header_mode) # ARGN: .s80 files
-  set(z80_out_dir "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${target}.dir/z80")
+  md_target_z80_sources_with_inc(${target} ${target} ${header_mode} ${ARGN})
+endfunction()
+
+function(md_target_z80_sources_with_inc target inc_target header_mode) # ARGN: .s80 files
+  set(z80_out_dir "${CMAKE_CURRENT_BINARY_DIR}/Intermediates/${target}.z80")
 
   set(processed_src)
   set(processed_headers)
@@ -16,11 +20,16 @@ function(md_target_z80_sources target header_mode) # ARGN: .s80 files
     set(z80_bin ${out_stem}.o80)
     # message("Building to ${m68k_asm}")
 
+    set(target_includes "$<LIST:TRANSFORM,$<TARGET_PROPERTY:${target},INCLUDE_DIRECTORIES>,PREPEND,-i>")
+    set(inc_target_includes "$<LIST:TRANSFORM,$<TARGET_PROPERTY:${inc_target},INCLUDE_DIRECTORIES>,PREPEND,-i>")
+    set(use_inc_target_includes "$<NOT:$<STREQUAL:${target},${inctarget}>>")
     add_custom_command(
       OUTPUT ${m68k_asm} ${c_header}
-      COMMAND ${ASMZ80_CMD} -i${SGDK}/inc -i${SGDK}/inc/snd ${z80_source} ${z80_bin} &&
+      COMMAND ${ASMZ80_CMD} ${target_includes} $<${use_inc_target_includes}:${inc_target_includes}> ${z80_source} ${z80_bin} &&
               ${BINTOS_CMD} ${z80_bin} ${m68k_asm}
-      DEPENDS ${z80_source} bintos # sjasm
+      COMMAND_EXPAND_LISTS
+      VERBATIM
+      DEPENDS ${z80_source} bintos sjasm
       BYPRODUCTS ${z80_bin}
     )
 
@@ -28,11 +37,10 @@ function(md_target_z80_sources target header_mode) # ARGN: .s80 files
     list(APPEND processed_headers ${c_header})
   endforeach()
 
-  target_include_directories(${target} PRIVATE ${z80_out_dir})
   target_sources(${target} PRIVATE ${processed_src})
   target_sources(${target}
     ${header_mode}
-    FILE_SET z80_headers TYPE HEADERS BASE_DIRS "${z80_out_dir}" FILES
+    FILE_SET z80_headers TYPE HEADERS BASE_DIRS ${z80_out_dir} FILES
       ${processed_headers}
   )
 
@@ -40,7 +48,7 @@ endfunction()
 
 # Rules to build resources
 function(md_target_res_sources target header_mode) # ARGN: .res files
-  set(res_out_dir "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${target}.dir/res")
+  set(res_out_dir "${CMAKE_CURRENT_BINARY_DIR}/Intermediates/${target}.res")
 
   set(processed_src)
   set(processed_headers)
@@ -51,19 +59,21 @@ function(md_target_res_sources target header_mode) # ARGN: .res files
     set(out_stem ${res_out_dir}/${res_relative_stem})
     set(m68k_asm ${out_stem}.s)
     set(c_header ${out_stem}.h)
+    set(dep_file ${out_stem}.d)
     # message("Building to ${m68k_asm}")
 
     add_custom_command(
       OUTPUT ${m68k_asm} ${c_header}
-      COMMAND ${RESCOMP_CMD} ${res_source} ${m68k_asm}
+      DEPFILE ${dep_file}
+      COMMAND ${RESCOMP_CMD} ${res_source} ${m68k_asm} -dep ${dep_file}
       DEPENDS ${res_source} xgmtool
+      BYPRODUCTS ${dep_file}
     )
 
     list(APPEND processed_src ${m68k_asm})
     list(APPEND processed_headers ${c_header})
   endforeach()
 
-  target_include_directories(${target} PRIVATE ${res_out_dir})
   target_sources(${target} PRIVATE ${processed_src})
   target_sources(${target}
     ${header_mode}
