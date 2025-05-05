a55c589fa2663ba1a4ee2aa0f28679441be7e02a
relick <relick@users.noreply.github.com>
2025-05-05 02:12:22 +0100

    gcc 15 fix - add __attribute__(()nonstring)) to the ROMHeader strings
    These shouldn't be null terminated, they should be fully filled.

--- inc/sys.h
+++ inc/sys.h
@@ -55,24 +55,24 @@
 // exist through rom_head.c
 typedef struct
 {
-    char console[16];               /* Console Name (16) */
-    char copyright[16];             /* Copyright Information (16) */
-    char title_local[48];           /* Domestic Name (48) */
-    char title_int[48];             /* Overseas Name (48) */
-    char serial[14];                /* Serial Number (2, 12) */
-    u16 checksum;                   /* Checksum (2) */
-    char IOSupport[16];             /* I/O Support (16) */
-    u32 rom_start;                  /* ROM Start Address (4) */
-    u32 rom_end;                    /* ROM End Address (4) */
-    u32 ram_start;                  /* Start of Backup RAM (4) */
-    u32 ram_end;                    /* End of Backup RAM (4) */
-    char sram_sig[2];               /* "RA" for save ram (2) */
-    u16 sram_type;                  /* 0xF820 for save ram on odd bytes (2) */
-    u32 sram_start;                 /* SRAM start address - normally 0x200001 (4) */
-    u32 sram_end;                   /* SRAM end address - start + 2*sram_size (4) */
-    char modem_support[12];         /* Modem Support (24) */
-    char notes[40];                 /* Memo (40) */
-    char region[16];                /* Country Support (16) */
+    char console[16]       __attribute__((nonstring));    /* Console Name (16) */
+    char copyright[16]     __attribute__((nonstring));    /* Copyright Information (16) */
+    char title_local[48]   __attribute__((nonstring));    /* Domestic Name (48) */
+    char title_int[48]     __attribute__((nonstring));    /* Overseas Name (48) */
+    char serial[14]        __attribute__((nonstring));    /* Serial Number (2, 12) */
+    u16 checksum;                                         /* Checksum (2) */
+    char IOSupport[16]     __attribute__((nonstring));    /* I/O Support (16) */
+    u32 rom_start;                                        /* ROM Start Address (4) */
+    u32 rom_end;                                          /* ROM End Address (4) */
+    u32 ram_start;                                        /* Start of Backup RAM (4) */
+    u32 ram_end;                                          /* End of Backup RAM (4) */
+    char sram_sig[2]       __attribute__((nonstring));    /* "RA" for save ram (2) */
+    u16 sram_type;                                        /* 0xF820 for save ram on odd bytes (2) */
+    u32 sram_start;                                       /* SRAM start address - normally 0x200001 (4) */
+    u32 sram_end;                                         /* SRAM end address - start + 2*sram_size (4) */
+    char modem_support[12] __attribute__((nonstring));    /* Modem Support (24) */
+    char notes[40]         __attribute__((nonstring));    /* Memo (40) */
+    char region[16]        __attribute__((nonstring));    /* Country Support (16) */
 } ROMHeader;
 
 extern const ROMHeader rom_header;
